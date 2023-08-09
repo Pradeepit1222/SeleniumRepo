b6e52937ce7b05af6a6145f81172f8aa955057ff
Pradeepit1222 <141850650+Pradeepit1222@users.noreply.github.com>
2023-08-09 20:47:43 +0530

    Add files via upload
    Week1

---
+++ week1/day1/Car.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Car {
+	
+	public static void main(String[] args) {
+		/*Need to  achieve
+		 * Try hard
+		 * 09/08/23
+		*/
+		System.out.println("Try untill you succeed-123@");
+	}
+
+}
