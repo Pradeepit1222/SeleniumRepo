be204e494f5d267e08bcfb2628534e3e83054738
Pradeepit1222 <141850650+Pradeepit1222@users.noreply.github.com>
2023-08-10 23:10:08 +0530

    Add files via upload

---
+++ NegativeOrPositive.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class NegativeOrPositive {
+
+	public static void main(String[] args) {
+		int i= -40;
+		 if(i<0)
+		 System.out.println("Number is negative");
+		 else if (i>0){
+			 System.out.println("Number is positive" );
+		 }
+		 
+		 
+				 
+		 }
+		// TODO Auto-generated method stub
+
+	
+
+}
