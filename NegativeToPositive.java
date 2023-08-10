78fd1ffe4ea78f39b6541ac17e5fd04491569151
Pradeepit1222 <141850650+Pradeepit1222@users.noreply.github.com>
2023-08-10 23:10:34 +0530

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		
+		int num = -40;
+		if (num<0)
+		{
+			num = -num ;
+			System.out.println("Output is :" + num );
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
